ff17fc3b57aabd8a8046a558367c8ec43ec69aa1
AyJayHam <Anton.wietzke@gmx.de>
2019-11-09 18:07:19 +0100

    update

--- elegal_assert.py
+++ elegal_assert.py
@@ -7,3 +7,13 @@ def testat_string(x):
         print("Der ausgegebene Text war leider falsch :(")
     elif (type(x) == type("test")) and (x.upper() == "eLegal ist toll!".upper()):
         print("Gross- und Kleinschreibung ist wichtig!")
+
+
+def klausurenschnitt_check(x):
+    print(x)
+    if x == 7:
+        print("Korrekt der Schnitt ist 7")
+        return
+    else:
+        print("Dein berechneter Schnitt war falsch")
+        return
